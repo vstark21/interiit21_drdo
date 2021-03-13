47b2c23d624ec60c79f2d8a412ec8ff4713ba8f8
vstark21 <chepurivishwas360@gmail.com>
2021-03-13 12:44:38 +0530

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,136 @@
-# interiit21_drdo
+﻿# Installation 
+## ROS
+You can find these installation instructions [here](wiki.ros.org/melodic/Installation/Ubuntu).
+#### Setup your sources.list
+```sh
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+```
+#### Set up your keys
+```sh
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+```
+#### Update packages and install ROS
+```sh
+sudo apt update
+sudo apt install ros-melodic-desktop-full
+```
+#### Setup the environment
+```sh
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc	
+```
+#### Dependencies
+```sh
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+```
+#### Rosdep
+```sh
+sudo apt install python-rosdep
+sudo rosdep init
+rosdep update
+```
+
+## Ardupilot
+### Installing Ardupilot and MAVProxy
+#### Clone ArduPilot
+
+In home directory:
+```sh
+cd ~
+sudo apt install git
+git clone https://github.com/ArduPilot/ardupilot.git
+cd ardupilot
+git checkout Copter-3.6
+git submodule update --init --recursive
+```
+
+#### Install dependencies:
+```sh
+sudo apt install python-matplotlib python-serial python-wxgtk3.0 python-wxtools python-lxml python-scipy python-opencv ccache gawk python-pip python-pexpect
+```
+
+#### Use pip (Python package installer) to install mavproxy:
+```sh
+sudo pip install future pymavlink MAVProxy
+```
+
+Open `~/.bashrc` for editing:
+```sh
+gedit ~/.bashrc
+```
+
+Add these lines to end of `~/.bashrc` (the file open in the text editor):
+```sh
+export PATH=$PATH:$HOME/ardupilot/Tools/autotest
+export PATH=/usr/lib/ccache:$PATH
+```
+
+Save and close the text editor.
+
+Reload `~/.bashrc`:
+```sh
+. ~/.bashrc
+```
+
+Run SITL (Software In The Loop) once to set params:
+```sh
+cd ~/ardupilot/ArduCopter
+sim_vehicle.py -w
+```
+## Gazebo and Plugins
+#### Gazebo
+
+Setup your computer to accept software from http://packages.osrfoundation.org:
+```sh
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+```
+
+Setup keys:
+```sh
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+```
+
+Reload software list:
+```sh
+sudo apt update
+```
+Install Gazebo:
+```sh
+sudo apt install gazebo9 libgazebo9-dev
+```
+### Install Gazebo plugin for APM (ArduPilot Master) :
+```sh
+cd ~
+git clone https://github.com/khancyr/ardupilot_gazebo.git
+cd ardupilot_gazebo
+git checkout dev
+```
+build and install plugin
+```sh
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install
+```
+```sh
+echo 'source /usr/share/gazebo/setup.sh' >> ~/.bashrc
+```
+Set paths for models:
+```sh
+echo 'export GAZEBO_MODEL_PATH=~/ardupilot_gazebo/models' >> ~/.bashrc
+. ~/.bashrc
+```
+
+#### Run Simulator
+In one Terminal (Terminal 1), run Gazebo:
+```sh
+gazebo --verbose ~/ardupilot_gazebo/worlds/iris_arducopter_runway.world
+```
+
+In another Terminal (Terminal 2), run SITL:
+```sh
+cd ~/ardupilot/ArduCopter/
+sim_vehicle.py -v ArduCopter -f gazebo-iris --console
+```
+
---
+++ scripts/nav.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python
+
+import rospy
+from mavros_msgs.msg import State, PositionTarget
+from mavros_msgs.srv import CommandTOL, CommandBool, SetMode
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose, Point, PoseStamped, Twist, TwistStamped
+from sensor_msgs.msg import Image
+import math, cv2
+import threading
+import sys, select, termios, tty
+import tf
+from cv_bridge import CvBridge
+
+pi_2 = math.pi / 2.0
+global KILL_THREAD
+KILL_THREAD = False
+
+class Controller:
+
+    def __init__(self):
+        rospy.init_node('control_node')
+        rospy.Subscriber("/mavros/state", State, self.state_callback)
+        rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self.pos_callback)
+        rospy.Subscriber("/depth_camera/rgb/image_raw", Image, self.dpcamrgb_callback)
+        rospy.Subscriber("/depth_camera/depth/image_raw", Image, self.dpcam_callback)
+
+        self.cmd_pos_pub = rospy.Publisher("/mavros/setpoint_position/local", PoseStamped, queue_size=1)
+        self.cmd_vel_pub = rospy.Publisher("/mavros/setpoint_velocity/cmd_vel_unstamped", Twist, queue_size=1)
+
+        self.mode_service = rospy.ServiceProxy("/mavros/set_mode", SetMode)
+        self.arm_service = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
+        self.takeoff_service = rospy.ServiceProxy('/mavros/cmd/takeoff', CommandTOL)
+
+        self.pose = Pose()
+        self.state = State()
+        self.timestamp = rospy.Time()
+        self.bridge = CvBridge()
+
+    def state_callback(self, data):
+        self.state = data
+
+    def pos_callback(self, data):
+        self.timestamp = data.header.stamp
+        self.pose = data.pose
+    
+    def dpcamrgb_callback(self, data):
+        try:
+            bridge = CvBridge()
+            image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')[:, :, ::-1]
+            cv2.imshow("Forward_rgb", image)
+            cv2.waitKey(1)
+        except Exception as e:
+            rospy.loginfo(e)
+    
+    def dpcam_callback(self, data):
+        try:
+            bridge = CvBridge()
+            image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+            cv2.imshow("Forward_depth", image)
+            cv2.waitKey(1)
+        except Exception as e:
+            rospy.loginfo(e)
+    
+    def goto(self, pose):
+        pose_stamped = PoseStamped()
+        pose_stamped.header.stamp = self.timestamp
+        pose_stamped.pose = pose
+
+        self.cmd_pos_pub.publish(pose_stamped)
+
+    def goto_xyz_rpy(self, x, y, z, ro, pi, ya):
+        pose = Pose()
+        pose.position.x = x
+        pose.position.y = y
+        pose.position.z = z
+
+        quats = tf.transformations.quaternion_from_euler(ro, pi, ya + pi_2)
+
+        pose.orientation.x = quats[0]
+        pose.orientation.y = quats[1]
+        pose.orientation.z = quats[2]
+        pose.orientation.w = quats[3]
+        self.goto(pose)
+
+    def set_vel(self, vx, vy, vz, avx=0, avy=0, avz=0):
+
+        vel = Twist()
+
+        vel.linear.x = vx
+        vel.linear.y = vy
+        vel.linear.z = vz
+
+        vel.angular.x = avx
+        vel.angular.y = avy
+        vel.angular.z = avz
+
+        self.cmd_vel_pub.publish(vel)
+
+    def moveF(self, vel):
+        quats = [self.pose.orientation.w,
+                self.pose.orientation.x,
+                self.pose.orientation.y,
+                self.pose.orientation.z]
+        theta, _, _ = tf.transformations.euler_from_quaternion(quats)
+        vx = -vel * math.cos(theta)
+        vy = vel * math.sin(theta)
+        self.set_vel(vx, vy, 0)
+
+    def moveB(self, vel):
+        quats = [self.pose.orientation.w,
+                self.pose.orientation.x,
+                self.pose.orientation.y,
+                self.pose.orientation.z]
+        theta, _, _ = tf.transformations.euler_from_quaternion(quats)
+        vx = vel * math.cos(theta)
+        vy = -vel * math.sin(theta)
+        self.set_vel(vx, vy, 0)
+
+    def moveL(self, vel):
+        quats = [self.pose.orientation.w,
+                self.pose.orientation.x,
+                self.pose.orientation.y,
+                self.pose.orientation.z]
+        theta, _, _ = tf.transformations.euler_from_quaternion(quats)
+        vx = -vel * math.sin(theta)
+        vy = -vel * math.cos(theta)
+        self.set_vel(vx, vy, 0)
+
+    def moveR(self, vel):
+        quats = [self.pose.orientation.w,
+                self.pose.orientation.x,
+                self.pose.orientation.y,
+                self.pose.orientation.z]
+        theta, _, _ = tf.transformations.euler_from_quaternion(quats)
+        vx = vel * math.sin(theta)
+        vy = vel * math.cos(theta)
+        self.set_vel(vx, vy, 0)
+
+    # Counter clockwise
+    def rotateCC(self, vel):
+        self.set_vel(0, 0, 0, avz=vel)
+
+    # Clockwise
+    def rotateC(self, vel):
+        self.set_vel(0, 0, 0, avz=-vel)
+
+    def stop(self):
+        self.set_vel(0, 0, 0)
+
+    def arm(self):
+
+        return self.arm_service(True)
+        
+    def disarm(self):
+
+        return self.arm_service(False)
+
+    def takeoff(self, height):
+
+        mode_resp = self.mode_service(custom_mode="4")
+        self.arm()
+
+        takeoff_resp = self.takeoff_service(altitude=height)
+        return takeoff_resp
+    
+    def land(self):
+
+        land_resp = self.mode_service(custom_mode="9")
+        self.disarm()
+    
+    def connect(self):
+
+        while not self.state.connected:
+            rospy.loginfo_once("Connecting to drone")
+            rospy.sleep(0.01)
+        
+        rospy.loginfo("Connected to Drone")
+
+        while self.state.mode != "GUIDED":
+            rospy.loginfo_once("Waiting for mode to set to `GUIDED`")
+            rospy.sleep(0.01)
+        
+        rospy.loginfo("Mode set to `GUIDED`")
+    
+def take_inputs(velocity, controller):
+    msg = """
+        Reading from the keyboard  and Publishing to Drone!
+        ---------------------------
+        Moving around:
+                 w
+        z    a       d   x
+                 s
+        
+        w : Move Forward
+        s : Move Backward
+        a : Move Left
+        d : Move Right
+        z : rotate counter-clockwise
+        x : rotate clockwise
+        space : STOP
+
+        CTRL-C to quit
+        """
+    print msg
+    settings = termios.tcgetattr(sys.stdin)
+
+    def getKey(key_timeout):
+        tty.setraw(sys.stdin.fileno())
+        rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
+        if rlist:
+            key = sys.stdin.read(1)
+        else:
+            key = ''
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        return key
+
+    while not rospy.is_shutdown() and not KILL_THREAD:
+        x = getKey(0.1)
+        if x == "w" or x == "W":
+            controller.moveF(velocity)
+        elif x == "s" or x == "S":
+            controller.moveB(velocity)
+        elif x == "a" or x == "A":
+            controller.moveL(velocity)
+        elif x == "d" or x == "D":
+            controller.moveR(velocity)
+        elif x == "z" or x == "Z":
+            controller.rotateCC(velocity)
+        elif x == "x" or x == "X":
+            controller.rotateC(velocity)
+        elif x == " ":
+            controller.stop()
+        elif x == '\x03':
+            break
+
+
+if __name__ == "__main__":
+
+    cont = Controller()
+
+    cont.connect()
+    cont.takeoff(4)
+
+    input_thread = threading.Thread(target=take_inputs, args=(1,cont))
+    input_thread.start()
+
+    rate = rospy.Rate(10)
+
+    try:
+        while not rospy.is_shutdown():
+            rate.sleep()
+    except KeyboardInterrupt:
+        rospy.loginfo("Manual Interruption Occured")
+        KILL_THREAD = True
+        cv2.destroyAllWindows()
+        input_thread.join()
