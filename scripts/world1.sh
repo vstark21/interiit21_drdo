513f2418b7d5f7b0a1a5935375195cf3463a9369
vstark21 <chepurivishwas360@gmail.com>
2021-03-23 20:52:05 +0530

    Added comments in nav.py

--- scripts/world1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gnome-terminal -- roslaunch interiit21_drdo world1.launch &&
-gnome-terminal -- rosrun interiit21_drdo nav.py && 
-cd ~/ardupilot/ArduCopter/ && sim_vehicle.py -v ArduCopter -f gazebo-iris --console
-
-
-
-
